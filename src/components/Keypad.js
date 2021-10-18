5379c4d744a565449eb6417d768f4b6acf124c9e
Sam Bencivengo <sambencivengo@gmail.com>
2021-10-18 19:45:25 -0400

    commit tree 5ee0c2f0661e34ac97c933d495adc1ff71e89de2

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import react from 'react';
+
+const Keypad = () => {
+	const handleSubmit = (e) => {
+		console.log('Entering password...');
+	};
+	return (
+		<>
+			<form onChange={handleSubmit}>
+				<input type="password"></input>
+			</form>
+		</>
+	);
+};
+
+export default Keypad;
